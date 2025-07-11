647fa5e656c9111e67b71aa0d3df664de8bc8790
Bakytzhan Temirgali <ba.temirgali@gmail.com>
2025-07-11 13:24:06 +0500

    Add metadata

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,10 @@ import "./globals.css";
 import Header from "@/app/components/header/Header";
 import Footer from "@/app/components/footer/Footer";
 
+export const metadata = {
+  title: "Animal Shelter",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
