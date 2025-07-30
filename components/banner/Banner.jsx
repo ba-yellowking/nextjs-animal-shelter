df288007d8b1247e66dc25ea1309a7a54749a904
Bakytzhan Temirgali <ba.temirgali@gmail.com>
2025-07-30 14:05:13 +0500

    Style banner component

--- components/banner/Banner.jsx
+++ components/banner/Banner.jsx
@@ -5,11 +5,10 @@ export default function Banner() {
   return (
     <div className={classes.banner}>
       <div className={classes.bannerTitle}>
-        <h1>Find a Friend – Save a Life</h1>
+        <h1>Find your new best friend!</h1>
         <div className={classes.description}>
           <p>Welcome to Animal Shelter! A place where love finds a home.</p>
-          <p>Visit the shelter or explore our online gallery.</p>
-          <p>Adopt, foster, or share. Help us make a difference.</p>
+          <p>Learn, care, and help us make a difference.</p>
         </div>
       </div>
 
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,41 +1,32 @@
 import classes from "./Header.module.css";
 import Link from "next/link";
-// import Image from "next/image";
-// import logo from "@/assets/logo.png"; // '@/assets/logo.png' если alias настроен
-import pawTrace from "@/assets/pawTrace.png";
 import ActiveLink from "../../ui/active-link/ActiveLink";
-import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={classes.header}>
       <div className={classes.content}>
-        <div className={classes.icon}>
-          <Link href="/">
-            <p className={classes.iconText}>ANIMAL</p>
-            <svg
-              className={classes.pawTrace}
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M442.8 361.82c-8.8-25.1-29.31-37.82-49.11-50.12c-17.23-10.71-33.5-20.83-44.14-39c-29.33-50.33-45.44-80.7-93.49-80.7s-64.21 30.38-93.61 80.69c-10.65 18.21-27 28.35-44.25 39.08c-19.8 12.31-40.27 25-49.1 50.05a78.1 78.1 0 0 0-5.1 28.29C64 430.85 96.45 464 132.4 464s83.31-18.13 123.76-18.13S343.31 464 379.71 464S448 430.85 448 390.11a78.3 78.3 0 0 0-5.2-28.29"
-              />
-              <ellipse cx="72" cy="216" fill="currentColor" rx="56" ry="72" />
-              <ellipse cx="184" cy="120" fill="currentColor" rx="56" ry="72" />
-              <ellipse cx="328" cy="120" fill="currentColor" rx="56" ry="72" />
-              <ellipse cx="440" cy="216" fill="currentColor" rx="56" ry="72" />
-            </svg>
-            <p className={classes.iconText}>SHELTER</p>
-          </Link>
-
-          {/*<nav className={classes.contacts}>*/}
-          {/*  <ActiveLink href="/contacts">Contacts</ActiveLink>*/}
-          {/*</nav>*/}
-        </div>
+        <Link href="/" className={classes.logo}>
+          ANIMAL
+          <br />
+          SHELTER
+          <svg
+            className={classes.pawTrace}
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M442.8 361.82c-8.8-25.1-29.31-37.82-49.11-50.12c-17.23-10.71-33.5-20.83-44.14-39c-29.33-50.33-45.44-80.7-93.49-80.7s-64.21 30.38-93.61 80.69c-10.65 18.21-27 28.35-44.25 39.08c-19.8 12.31-40.27 25-49.1 50.05a78.1 78.1 0 0 0-5.1 28.29C64 430.85 96.45 464 132.4 464s83.31-18.13 123.76-18.13S343.31 464 379.71 464S448 430.85 448 390.11a78.3 78.3 0 0 0-5.2-28.29"
+            />
+            <ellipse cx="72" cy="216" fill="currentColor" rx="56" ry="72" />
+            <ellipse cx="184" cy="120" fill="currentColor" rx="56" ry="72" />
+            <ellipse cx="328" cy="120" fill="currentColor" rx="56" ry="72" />
+            <ellipse cx="440" cy="216" fill="currentColor" rx="56" ry="72" />
+          </svg>
+        </Link>
 
         <div className={classes.navigation}>
           <ActiveLink href="/about">About us</ActiveLink>
@@ -43,22 +34,22 @@ export default function Header() {
           <a>How Can I Help?</a>
         </div>
 
-        <div className={classes.right}>
-          <svg
-            className={classes.click}
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-          >
-            <path
-              fill="currentColor"
-              d="M220.49 190.83a12 12 0 0 1 0 17l-12.69 12.66a12 12 0 0 1-17 0l-56.56-56.57L115 214.09c0 .1-.08.21-.13.32a15.83 15.83 0 0 1-14.6 9.59h-.79a15.83 15.83 0 0 1-14.41-11L32.8 52.92A16 16 0 0 1 52.92 32.8L213 85.07a16 16 0 0 1 1.41 29.8l-.32.13l-50.17 19.27ZM96 32a8 8 0 0 0 8-8v-8a8 8 0 0 0-16 0v8a8 8 0 0 0 8 8m-80 72h8a8 8 0 0 0 0-16h-8a8 8 0 0 0 0 16m108.42-64.84a8 8 0 0 0 10.74-3.58l8-16a8 8 0 0 0-14.31-7.16l-8 16a8 8 0 0 0 3.57 10.74m-96 81.69l-16 8a8 8 0 0 0 7.16 14.31l16-8a8 8 0 1 0-7.16-14.31"
-            />
-          </svg>
+        <div className={classes.adoption}>
           <Link href="/animals" className={classes.cta}>
             ADOPT
             <br />A FRIEND
+            <svg
+              className={classes.click}
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 256 256"
+            >
+              <path
+                fill="currentColor"
+                d="M220.49 190.83a12 12 0 0 1 0 17l-12.69 12.66a12 12 0 0 1-17 0l-56.56-56.57L115 214.09c0 .1-.08.21-.13.32a15.83 15.83 0 0 1-14.6 9.59h-.79a15.83 15.83 0 0 1-14.41-11L32.8 52.92A16 16 0 0 1 52.92 32.8L213 85.07a16 16 0 0 1 1.41 29.8l-.32.13l-50.17 19.27ZM96 32a8 8 0 0 0 8-8v-8a8 8 0 0 0-16 0v8a8 8 0 0 0 8 8m-80 72h8a8 8 0 0 0 0-16h-8a8 8 0 0 0 0 16m108.42-64.84a8 8 0 0 0 10.74-3.58l8-16a8 8 0 0 0-14.31-7.16l-8 16a8 8 0 0 0 3.57 10.74m-96 81.69l-16 8a8 8 0 0 0 7.16 14.31l16-8a8 8 0 1 0-7.16-14.31"
+              />
+            </svg>
           </Link>
         </div>
       </div>
